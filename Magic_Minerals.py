3feb062bb37783740f3e7febe53f031e9d4190fa
DenisAvocado <shelden2005@mail.ru>
2022-01-06 18:17:19 +0600

    6th January update

--- Magic_Minerals.py
+++ Magic_Minerals.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import os
+from copy import deepcopy
 
 pygame.init()
 FPS = 50
@@ -8,18 +9,22 @@ WIDTH = 1080
 HEIGHT = 720
 STEP = 10
 
+first_time = True
+
 # группы спрайтов
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Magic Minerals')
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
+stones_group = pygame.sprite.Group()
+necessary_stones = []  # список необходимых камней (объектов класса NecessaryStone)
 necessary_stones_group = pygame.sprite.Group()
+instruments_group = pygame.sprite.Group()
 
 
-def load_image(name, color_key=None):
-    full_name = os.path.join('stones', name)
+def load_image(name,  directory='stones', color_key=None):
+    full_name = os.path.join(directory, name)
     try:
         image = pygame.image.load(full_name)
     except pygame.error as message:
@@ -52,7 +57,7 @@ def generate_level(level):
     x, y = None, None
     for y in range(len(level)):
         for x in range(len(level[y])):
-            Tile(level[y][x], x, y)
+            Stone(level[y][x], x, y)
     return x, y
 
 
@@ -62,24 +67,47 @@ def terminate():
 
 
 def draw_cell_field():
-    for y in range(6):
-        for x in range(6):
-            pygame.draw.rect(screen, pygame.Color('white'),
-                             (10 + x * tile_width,
-                              10 + y * tile_height,
-                              tile_width, tile_height), 1)
+    board.render(screen)
+    stones_group.draw(screen)
+
+
+def draw_instruments():
+    pygame.draw.rect(screen, pygame.Color('brown'), (630, 130, 90, 360), 0)
+    pygame.draw.rect(screen, pygame.Color('wheat'), (630, 130, 90, 360), 3)
+    for i in range(4):
+        pygame.draw.ellipse(screen, pygame.Color('lightsalmon'), (630, 130 + 90 * i, 90, 90), 0)
+        pygame.draw.ellipse(screen, pygame.Color('wheat'), (630, 130 + 90 * i, 90, 90), 2)
+        Instrument(instruments[i], 0, i)
+    instruments_group.draw(screen)
+
+
+def to_statistic(stone_num, quantity):
+    for st in necessary_stones:
+        if st.tile_type == stone_num:
+            st.text[0] += quantity
 
 
 def write_statistic(*stones):
     text = ''
-    x, y = 0, 6
-    for stone in stones:
-        NecessaryStone(stone[0], x, y)
-        text += f'          0/{stone[1]}'
-        x += 1.3
+    x, y = 0, 8
+    coeff = 10
+    global first_time
+    if first_time:
+        for stone in stones:
+            st = NecessaryStone(stone[0], x, y, stone[1])
+            necessary_stones.append(st)
+            text += ' ' * coeff + f'0/{stone[1]}'
+            x += 2
+            coeff = 19
+            first_time = False
+    else:
+        for stone in necessary_stones:
+            text += ' ' * coeff + f'{stone.text[0]}/{stone.text[1]}'
+            x += 2
+            coeff = 19
     text = [text]
     font = pygame.font.Font(None, 30)
-    text_coord = 500
+    text_coord = 650
     necessary_stones_group.draw(screen)
     for line in text:
         string_rendered = font.render(line, 1, pygame.Color('white'))
@@ -91,45 +119,248 @@ def write_statistic(*stones):
         screen.blit(string_rendered, text_rect)
 
 
-tile_images = {
+stone_images = {
+    '0': load_image('tiger.png'),
     '1': load_image('amber.png'),
     '2': load_image('amethyst.png'), '3': load_image('diamond.png'),
     '4': load_image('emerald.png'), '5': load_image('ruby.png'),
     '6': load_image('sapphire.png'),
 }
-for i in range(1, 7):
-    tile_images[str(i)] = pygame.transform.scale(tile_images[str(i)], (75, 75))
+
+instruments = ['pickaxe', 'drill', 'dynamite', 'lantern']
+instrument_images = {}
+
+for i in range(0, 7):
+    stone_images[str(i)] = pygame.transform.scale(stone_images[str(i)], (75, 75))
+
+for ins in instruments:
+    instrument_images[ins] = load_image(f'{ins}.png', directory='assets/instruments')
+    instrument_images[ins] = pygame.transform.scale(instrument_images[ins], (90, 90))
+
 
 tile_width = tile_height = 75
 
 
-class Tile(pygame.sprite.Sprite):
+class Board:
+    def __init__(self, width, height,
+                 left=10, top=10, cell_size=30):
+        self.width = width
+        self.height = height
+        self.board = load_level('level.txt')
+        self.cell_size = cell_size
+        self.left = left
+        self.top = top
+        self.c1 = (None, None)
+        self.c2 = (None, None)
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                if (x, y) == self.c1:
+                    pygame.draw.rect(screen, pygame.Color('lightgreen'),
+                                     (self.left + self.c1[0] * self.cell_size,
+                                      self.top + self.c1[1] * self.cell_size, self.cell_size, self.cell_size), 0)
+                if (x, y) == self.c2:
+                    pygame.draw.rect(screen, pygame.Color('pink'),
+                                     (self.left + self.c2[0] * self.cell_size,
+                                      self.top + self.c2[1] * self.cell_size, self.cell_size, self.cell_size), 0)
+                pygame.draw.rect(screen, pygame.Color('white'),
+                                 (self.left + x * self.cell_size,
+                                  self.top + y * self.cell_size,
+                                  self.cell_size, self.cell_size), 1)
+
+    def on_click(self, cell):
+        if self.c1 == (None, None) and self.c2 == (None, None):
+            self.c1 = cell
+        elif self.c1 != (None, None) and self.c2 == (None, None):
+            self.c2 = cell
+        else:
+            self.c1 = cell
+            self.c2 = (None, None)
+        if self.c1 != (None, None) and self.c2 != (None, None) and self.c1 != self.c2:
+            [st.kill() for st in stones_group]
+            c1, c2 = self.c1, self.c2
+            # for i, st in enumerate(stones_group):
+            #     if i == c1[0] * 8 + c1[1]:
+            #         st.kill()
+            #     if i == c2[0] * 8 + c2[1]:
+            #         st.kill()
+            if c1[1] == c2[1] and abs(c1[0] - c2[0]) == 1:
+                line1 = list(self.board[c1[1]])
+                stone1, stone2 = line1[c1[0]], line1[c2[0]]
+                if c1[0] < c2[0]:
+                    del line1[c1[0]]
+                    del line1[c2[0] - 1]
+                    line1.insert(c2[0] - 1, stone1)
+                    line1.insert(c1[0], stone2)
+                else:
+                    del line1[c2[0]]
+                    del line1[c1[0] - 1]
+                    line1.insert(c1[0] - 1, stone2)
+                    line1.insert(c2[0], stone1)
+                line1 = ''.join(line1)
+                self.board[self.c1[1]] = line1
+                self.horizontal_reduce()
+                self.vertical_reduce()
+            elif c1[0] == c2[0] and abs(c1[1] - c2[1]) == 1:
+                line1, line2 = list(self.board[c1[1]]), list(self.board[c2[1]])
+                stone1, stone2 = line1[c1[0]], line2[c2[0]]
+                del line1[c1[0]]
+                del line2[c2[0]]
+                line1.insert(c2[0], stone2)
+                line2.insert(c1[0], stone1)
+                line1, line2 = ''.join(line1), ''.join(line2)
+                self.board[c1[1]], self.board[c2[1]] = line1, line2
+                self.horizontal_reduce()
+                self.vertical_reduce()
+            move_pad.minus()
+
+    def get_cell(self, mouse_pos):
+        cell_x = (mouse_pos[0] - self.left) // self.cell_size
+        cell_y = (mouse_pos[1] - self.top) // self.cell_size
+        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
+            return None
+        return cell_x, cell_y
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
+
+    def horizontal_reduce(self):
+        i, j = 0, 0
+        while i < self.height:
+            del_list = []
+            while j < self.width:
+                cur_st = self.board[i][j]
+                del_list.append((i, j))
+                j += 1
+                if j == self.width:
+                    break
+                if self.board[i][j] == cur_st:
+                    while self.board[i][j] == cur_st:
+                        del_list.append((i, j))
+                        j += 1
+                        if j == self.width:
+                            break
+                    if len(del_list) >= 3:
+                        to_statistic(cur_st, len(del_list))
+                else:
+                    cur_st = self.board[i][j]
+                    del_list = []
+                if len(del_list) >= 3:
+                    tmp_line = list(self.board[i])
+                    for tpl in del_list:
+                        tmp_line[tpl[1]] = '0'
+                    tmp_line = ''.join(tmp_line)
+                    self.board[i] = tmp_line
+                    del_list = []
+            i += 1
+            j = 0
+
+    def vertical_reduce(self):
+        i, j = 0, 0
+        while j < self.height:
+            del_list = []
+            while i < self.width:
+                cur_st = self.board[i][j]
+                del_list.append((i, j))
+                i += 1
+                if i == self.height:
+                    break
+                if self.board[i][j] == cur_st:
+                    while self.board[i][j] == cur_st:
+                        del_list.append((i, j))
+                        i += 1
+                        if i == self.height:
+                            break
+                    if len(del_list) >= 3:
+                        to_statistic(cur_st, len(del_list))
+                else:
+                    cur_st = self.board[i][j]
+                    del_list = []
+                if len(del_list) >= 3:
+                    # tmp_line = list(self.board[i])
+                    for tpl in del_list:
+                        tmp_line = list(self.board[tpl[0]])
+                        tmp_line[tpl[1]] = '0'
+                        tmp_line = ''.join(tmp_line)
+                        self.board[tpl[0]] = tmp_line
+                    del_list = []
+            j += 1
+            i = 0
+
+
+class Stone(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
+        super().__init__(stones_group, all_sprites)
+        self.image = stone_images[tile_type]
         self.rect = self.image.get_rect().move(
             10 + tile_width * pos_x, 10 + tile_height * pos_y)
 
 
 class NecessaryStone(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
+    def __init__(self, tile_type, pos_x, pos_y, need):
         super().__init__(necessary_stones_group, all_sprites)
-        self.image = tile_images[tile_type]
+        self.tile_type = tile_type
+        self.image = stone_images[tile_type]
+        self.text = [0, need]
         self.rect = self.image.get_rect().move(
             10 + tile_width * pos_x, 10 + tile_height * pos_y)
 
 
-level_x, level_y = generate_level(load_level('level.txt'))
+class Instrument(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(instruments_group, all_sprites)
+        self.image = instrument_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            630, 130 + 90 * pos_y)
 
+
+class Move:
+    def __init__(self, n):
+        self.n = n
+
+    def minus(self):
+        self.n -= 1
+
+    def show(self):
+        pygame.draw.ellipse(screen, 'YellowGreen', (630, 10, 90, 90), 0)
+        pygame.draw.ellipse(screen, 'DarkGreen', (630, 10, 90, 90), 5)
+        text = [str(self.n)]
+        font = pygame.font.Font(None, 70)
+        text_coord = 17
+        for line in text:
+            string_rendered = font.render(line, 1, pygame.Color('DarkGreen'))
+            text_rect = string_rendered.get_rect()
+            text_coord += 10
+            text_rect.top = text_coord
+            text_rect.x = 645
+            text_coord += text_rect.height
+            screen.blit(string_rendered, text_rect)
+
+
+board = Board(8, 8, 10, 10, 75)
+move_pad = Move(20)
 running = True
 while running:
+    level_x, level_y = generate_level(board.board)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.fill('black')
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            board.get_click(event.pos)
     draw_cell_field()
-    write_statistic(('5', 10), ('6', 15), ('1', 20))
-    tiles_group.draw(screen)
+    draw_instruments()
+    move_pad.show()
+    write_statistic(('5', 10), ('6', 15), ('4', 20))
     pygame.display.flip()
+    screen.fill('black')
     clock.tick(FPS)
 terminate()
+
